30ca803fff39e5a2f6fc1c986cd9762c95841b3f
둥둥둥오리 <62438578+hanseu9839@users.noreply.github.com>
2021-02-09 01:36:00 +0900

    bakejoon-star

---
+++ bakejoon2439/star2439.java
@@ -0,0 +1,20 @@
+package bakejoon2439;
+
+import java.util.Scanner;
+
+public class star2439 {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        int nStar = scan.nextInt();
+        scan.close();
+        for (int i = 0; i < nStar; i++) {
+            for (int j = 0; j < nStar - i - 1; j++)// 5-0-1 4
+                System.out.print(" ");
+            for (int k = 0; k <= i; k++) // j=0 i=1 2 3 4 5
+            {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
