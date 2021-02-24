a500ad9ce5717fbf75fa13ad6af99f762f9aae4a
둥둥둥오리 <62438578+hanseu9839@users.noreply.github.com>
2021-02-25 05:03:05 +0900

    D:)error__Array

--- bakejoon10818/Main.java
+++ bakejoon10818/Main.java
@@ -8,25 +8,22 @@ public class Main {
         int n = sc.nextInt();
         int[] num = new int[n];
 
-        for(int i=0;i<n;i++)
-        {
-            num[i]= sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            num[i] = sc.nextInt();
         }
 
         int min = num[0];
         int max = num[0];
-        for(int i=0;i<n;i++)
-        {
-            if(min>num[i])
-            {
-                min=num[i]
+        for (int i = 0; i < n; i++) {
+            if (min > num[i]) {
+                min = num[i];
             }
-            
-            if(max<num[i]){
-                max=num[i];
+
+            if (max < num[i]) {
+                max = num[i];
             }
         }
-
-        System.out.print("%d %d",min,max);
+        System.out.print(min + " " + max);
+        sc.close();
     }
 }
